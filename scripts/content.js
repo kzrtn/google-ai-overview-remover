9bf6e654519a0066abb4d83abb4c07745ecb5e17
Jolene T <kartistenn2@gmail.com>
2026-08-05 03:34:01 +0000

    separated features into separate functions

--- scripts/content.js
+++ scripts/content.js
@@ -1,24 +1,40 @@
+/** Global flags */
+let disabledAIModeTab = false
+let disabledAIOverview = false
+
 const observer = new MutationObserver((mutationList, observer) => {
+  if (!removeAIOverview) removeAIOverview()
+  if (!disabledAIModeTab) removeAIModeTab()
+  
+  removeAIfromPeopleAlsoAsk()
+})
+
+const removeAIOverview = () => {
   const AIOverview = document.getElementById('eKIzJc') || document.querySelector('[jsname="ZLxsqf"]')
   if (AIOverview) {
     AIOverview.remove()
+    disabledAIOverview = true
     console.log('Removed global AI Overview')
   }
+}
 
+const removeAIModeTab = () => {
   const AIModeTab = document.querySelector('[jsname="xBNgKe"]')
-  if (AIModeTab && !disabledAIModeTab) {
+  if (AIModeTab) {
     AIModeTab.style = "display: none" // using .remove() breaks the whole list
     disabledAIModeTab = true
     console.log('Removed AI mode tab')
   }
+}
 
-  /* 
-  AI Overview answers are class="r2fjmd t0bRye", while
-  non-AI answers are class="r2fjmd t0bRye Sbgr0".
-  It's very fragile to select by classes like this,
-  but I can't find any other way to differentiate between
-  the two without the user opening the tabs first
-  */
+/* 
+AI Overview answers are class="r2fjmd t0bRye", while
+non-AI answers are class="r2fjmd t0bRye Sbgr0".
+It's very fragile to select by classes like this,
+but I can't find any other way to differentiate between
+the two without the user opening the tabs first
+*/
+const removeAIfromPeopleAlsoAsk = () => {
   const peopleAsk = document.querySelector('[class="r2fjmd t0bRye"]')
   if (peopleAsk) {
     let peopleAskTabContainer = peopleAsk.closest('[jsname]')
@@ -33,8 +49,6 @@ const observer = new MutationObserver((mutationList, observer) => {
       console.log("FATAL ERROR: No parent container for AI suggested answer found.")
     }
   }
-})
+}
 
-let disabledAIModeTab = false;
-const config = { childList: true, subtree: true };
-observer.observe(document, config);
+observer.observe(document, { childList: true, subtree: true })
