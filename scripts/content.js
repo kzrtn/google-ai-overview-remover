acbfb29e1f2758f7542ed73db94b468674a5ac92
kzrtn <kartisten2@gmail.com>
2026-08-05 10:57:29 +0800

    added feature to remove AI mode tab and people also ask AI suggestions

--- scripts/content.js
+++ scripts/content.js
@@ -1,11 +1,32 @@
 const observer = new MutationObserver((mutationList, observer) => {
-  const target = document.getElementById('eKIzJc')
-  if (target) {
-    console.log('found element', target)
-    target.remove()
-    observer.disconnect()
+  const AIOverview = document.getElementById('eKIzJc') || document.querySelector('[jsname="ZLxsqf"]')
+  if (AIOverview) {
+    AIOverview.remove()
+    console.log('Removed global AI Overview')
+  }
+
+  const AIModeTab = document.querySelector('[jsname="xBNgKe"]')
+  if (AIModeTab && !disabledAIModeTab) {
+    AIModeTab.style = "display: none" // using .remove() breaks the whole list
+    disabledAIModeTab = true
+    console.log('Removed AI mode tab')
+  }
+
+  /* 
+  AI Overview answers are class="r2fjmd t0bRye", while
+  non-AI answers are class="r2fjmd t0bRye Sbgr0".
+  It's very fragile to select by classes like this,
+  but I can't find any other way to differentiate between
+  the two without the user opening the tabs first
+  */
+  const peopleAsk = document.querySelector('[class="r2fjmd t0bRye"]')
+  if (peopleAsk) {
+    const peopleAskTabContainer = peopleAsk.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement
+    peopleAskTabContainer.remove()
+    console.log("Removed AI suggested question and answer in 'People Also Ask'")
   }
 })
 
+let disabledAIModeTab = false;
 const config = { childList: true, subtree: true };
 observer.observe(document, config);
